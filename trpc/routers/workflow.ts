42e898284f3fb3114416a8a73449de04ebbda63f
ShreyasGadave <shreyasgadave777@gmail.com>
2026-02-13 22:56:58 +0530

    ai-sdk

--- trpc/routers/workflow.ts
+++ trpc/routers/workflow.ts
@@ -3,16 +3,21 @@ import { BaseProcedure, createTRPCRouter } from "../init";
 import { inngest } from "@/inngest/client";
 
 export const WorkFlowRouter = createTRPCRouter({
+  geminiTest: BaseProcedure.mutation(async () => {
+    await inngest.send({ name: "execute.ai" });
+    return { success: true, message: "Job Queued" };
+  }),
   getWorkflows: BaseProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
 
-  createWorkflow: BaseProcedure.mutation(async() => {
-    await inngest.send({name:'test/hello.world',
-        data:{
-            email: 'shreyasgadave@gmail.com'
-        }
-    })
+  createWorkflow: BaseProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "shreyasgadave@gmail.com",
+      },
+    });
     return prisma.workflow.create({
       data: {
         name: "test-workflow",
